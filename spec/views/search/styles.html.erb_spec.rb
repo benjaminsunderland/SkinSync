20de0acc2dc31074e11efab92ea5f5ea4e410b38
James Hughes <hugjimbo77@gmail.com>
2017-10-26 11:22:06 +0100

    tests added for search body_parts & styles pages : tests passing

--- spec/views/search/styles.html.erb_spec.rb
+++ spec/views/search/styles.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "search/styles.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe "search/styles.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+feature 'search_styles page' do
+  scenario 'should be able to view search tattoo styles page' do
+    visit 'search/styles'
+    expect(status_code).to eq(200)
+    expect(page).to have_content("What style?")
+  end
 end
